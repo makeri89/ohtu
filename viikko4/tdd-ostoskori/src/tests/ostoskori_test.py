101dbbeacd5bddd909b93d1c7b944875b875c783
Markus Kaihola <makeri89@hotmail.com>
2021-11-27 14:01:05 +0200

    added test for basket price with two different items

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -30,4 +30,12 @@ class TestOstoskori(unittest.TestCase):
         
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
         
-
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korin_hinta_on_oikein(self):
+        maito = Tuote('Maito', 3)
+        vichy = Tuote('Vichy', 4)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(vichy)
+        
+        tuotteiden_hinta = maito.hinta() + vichy.hinta()
+        
+        self.assertEqual(self.kori.hinta(), tuotteiden_hinta)
