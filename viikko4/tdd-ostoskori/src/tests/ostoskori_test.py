dec2a140546b0320c10ad93e5ab011cefac46e17
Markus Kaihola <makeri89@hotmail.com>
2021-11-27 14:23:27 +0200

    two different items add two Ostos objects to basket

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -6,6 +6,7 @@ class TestOstoskori(unittest.TestCase):
     def setUp(self):
         self.kori = Ostoskori()
         self.maito = Tuote('maito', 3)
+        self.vichy = Tuote('vichy', 4)
 
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.kori.hinta(), 0)
@@ -22,18 +23,16 @@ class TestOstoskori(unittest.TestCase):
         self.assertEqual(self.kori.hinta(), 3)
         
     def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
-        vichy = Tuote('Vichy', 4)
         self.kori.lisaa_tuote(self.maito)
-        self.kori.lisaa_tuote(vichy)
+        self.kori.lisaa_tuote(self.vichy)
         
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
         
     def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korin_hinta_on_oikein(self):
-        vichy = Tuote('Vichy', 4)
         self.kori.lisaa_tuote(self.maito)
-        self.kori.lisaa_tuote(vichy)
+        self.kori.lisaa_tuote(self.vichy)
         
-        tuotteiden_hinta = self.maito.hinta() + vichy.hinta()
+        tuotteiden_hinta = self.maito.hinta() + self.vichy.hinta()
         
         self.assertEqual(self.kori.hinta(), tuotteiden_hinta)
         
@@ -65,3 +64,11 @@ class TestOstoskori(unittest.TestCase):
         
         self.assertEqual(ostos.tuotteen_nimi(), self.maito.nimi())
         self.assertEqual(ostos.lukumaara(), 1)
+        
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskori_sisaltaa_kaksi_ostosta(self):
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.vichy)
+        
+        ostokset = self.kori.ostokset()
+        
+        self.assertEqual(len(ostokset), 2)
